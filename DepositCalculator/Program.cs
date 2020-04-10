4d229cb3acabef192e467266452420847e7f7fd2
Илья Казютин <gorgutz1@yandex.ru>
2020-04-10 12:51:16 +0300

    Добавьте файлы проекта.

---
+++ DepositCalculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepositCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите суммы вклада.");
+            var str = Console.ReadLine();
+            var AmountOfMoney = double.Parse(str);
+
+            Console.WriteLine("Введите годовой процент по вкладу.");
+            str = Console.ReadLine();
+            var AnnumInterest = double.Parse(str);
+
+            Console.WriteLine("Введите количество времени для вклада в месяцах.");
+            str = Console.ReadLine();
+            var NumberOfMonths = int.Parse(str);
+        }
+    }
+}
